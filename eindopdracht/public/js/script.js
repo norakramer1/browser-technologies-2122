41e523d929be4b041085fa490672deac8ca56b28
nora kramer <nora.kramer2@hva.nl>
2022-05-03 18:22:48 +0200

    added drag and drop func

--- eindopdracht/public/js/script.js
+++ eindopdracht/public/js/script.js
@@ -1,8 +1,36 @@
-const darkEl = document.querySelector('body');
-const buttonEl = document.querySelector('button.switch');
+//const darkEl = document.querySelector('body');
+// const buttonEl = document.querySelector('button.switch');
+// const draggables = document.querySelectorAll('.border');
+const draggables = document.querySelectorAll('.draggable');
+const dropZones = document.querySelectorAll('.drop-zone');
 
-function darkMode() {
-    darkEl.classList.toggle('darkmode')
-}
 
-buttonEl.addEventListener('click', darkMode)
+// function darkMode() {
+//     darkEl.classList.toggle('darkmode')
+// }
+
+draggables.forEach(draggable => {
+   //console.log(draggable);
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging')
+    })
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging')
+
+    })
+
+})
+
+console.log(dropZones)
+
+
+dropZones.forEach(dropZone => {
+    dropZone.addEventListener('dragover', e => {
+        e.preventDefault();
+        const draggableItem = document.querySelector('.dragging');
+        dropZone.appendChild(draggableItem)
+    })
+    //   draggables.remove();
+})
+
+// buttonEl.addEventListener('click', darkMode)
